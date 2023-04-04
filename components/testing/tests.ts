1023b0e1bd68856f81937bf9fc1fbb7115eec478
Chad Glazier <chadglazier@outlook.com>
2023-04-04 11:22:56 -0700

    updated the tests

--- components/testing/tests.ts
+++ components/testing/tests.ts
@@ -1,131 +1,150 @@
-import { ErrorMessageSchema, ExhibitCreatable, PopulatedExhibitSchema, UserDataSchema } from "@/types";
+import { ErrorMessageSchema, PopulatedExhibitCreatable, PopulatedExhibitSchema, UserDataSchema } from "@/types";
 import { ApiTestProps } from "./ApiTest";
 import { z } from "zod";
 
-const sampleExhibit: ExhibitCreatable = {
+const sampleExhibit: PopulatedExhibitCreatable = {
   title: "Sample's Title;",
   thumbnail: "thumbnail-1.png",
   summary: "Sample summary",
+  cards: [
+    {
+      media: "some-media.png",
+      description: "sample description and stuff."
+    },
+    {
+      media: "some-other-media.png",
+      description: "sample description number two."
+    }
+  ],
   published: false
-}
+};
 
-const tests: ApiTestProps[] = [
-  {
-    name: "Get Exhibit",
-    url: `/api/exhibit?title=`,
-    request: {
-      method: "GET"
+const sampleExhibitUpdated: PopulatedExhibitCreatable = {
+  title: "Sample's Title;",
+  thumbnail: "thumbnail-1.png",
+  summary: "Updated sample summary",
+  cards: [
+    {
+      media: "some-media.png",
+      description: "sample description and stuff."
     },
-    expectedReponseBodies: [
-      PopulatedExhibitSchema
-    ]
-  },
-  {
-    name: "Get Exhibits by ID List",
-    url: "/api/exhibit?id=1&id=2",
-    request: {
-      method: "GET"
+    {
+      media: "updated-some-other-media.png",
+      description: "updated sample description number two."
+    }
+  ],
+  published: false
+};
+
+const otherSampleExhibit: PopulatedExhibitCreatable = {
+  title: "Other's Title;",
+  thumbnail: "thumbnail-2.png",
+  summary: "Other sample summary",
+  cards: [
+    {
+      media: "some-other-media.png",
+      description: "Other sample description and stuff."
     },
-    expectedReponseBodies: [
-      z.array(PopulatedExhibitSchema)
-    ]
-  },
+    {
+      media: "some-other-other-media.png",
+      description: "Other sample description number two."
+    }
+  ],
+  published: false
+};
+
+const createTests: ApiTestProps[] = [
   {
-    name: "Get All Exhibits",
-    url: "/api/exhibit?id=*",
-    request: {
-      method: "GET"
-    },
-    expectedReponseBodies: [
-      z.array(PopulatedExhibitSchema)
-    ]
-  },
-  {
-    name: "Invalid User Authentication",
-    url: "/api/user/authenticate",
-    request: {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email: "incorrectEmail@gmail.com",
-        password: "wrongPassword",
-      }),
-    },
-    expectedReponseBodies: [ErrorMessageSchema],
-  },
-  {
-    name: "Verify Authenticated User",
-    url: "/api/user/authentic",
-    request: {
-      method: "GET",
-      credentials: "include",
-    },
-    expectedReponseBodies: [UserDataSchema],
-  },
-  {
-    name: "Verify Unauthenticated User",
-    url: "/api/user/authentic",
-    request: {
-      method: "GET",
-      credentials: "omit",
-    },
-    expectedReponseBodies: [ErrorMessageSchema],
-  },
-  {
-    name: "Post an Exhibit",
+    name: "Create a New Exhibit",
     url: "/api/exhibit",
     request: {
       method: "POST",
       credentials: "same-origin",
-      headers: { "Content-Type": "application/json"},
-      body: JSON.stringify({
-        "title": "Vernons Chinatown 3",
-        "thumbnail": "thumbnail3.jpg",
-        "summary": "Lorem ipsum dolor",
-        "published": true,
-        "cards": [
-          {
-            "media": "media.png",
-            "description": "a new picture ",
-          }
-        ]
-      })
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(sampleExhibit)
     },
     expectedReponseBodies: [PopulatedExhibitSchema]
   },
   {
-    name: "Put an Exhibit",
+    name: "Create or Replace an Exhibit",
+    url: "/api/exhibit",
+    request: {
+      method: "POST",
+      credentials: "same-origin",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(otherSampleExhibit)
+    },
+    expectedReponseBodies: [PopulatedExhibitSchema]
+  }
+];
+
+const readTests: ApiTestProps[] = [
+  {
+    name: "Get an Exhibit",
+    url: `/api/exhibit?title=${encodeURIComponent(sampleExhibit.title)}`,
+    request: {
+      method: "GET"
+    },
+    expectedReponseBodies: [PopulatedExhibitSchema]
+  },
+  {
+    name: "Get a List of Exhibits",
+    url: `/api/exhibit?title=${encodeURIComponent(sampleExhibit.title)}&title=${encodeURIComponent(otherSampleExhibit.title)}`,
+    request: {
+      method: "GET"
+    },
+    expectedReponseBodies: [z.array(PopulatedExhibitSchema)]
+  },
+  {
+    name: "Get All Exhibits",
+    url: "/api/exhibit?title=*",
+    request: {
+      method: "GET"
+    },
+    expectedReponseBodies: [z.array(PopulatedExhibitSchema)]
+  }
+];
+
+const updateTests: ApiTestProps[] = [
+  {
+    name: "Replace an Existing Exhibit",
     url: "/api/exhibit",
     request: {
       method: "PUT",
       credentials: "same-origin",
-      headers: { "Content-Type": "application/json"},
-      body: JSON.stringify({
-        "title": "Vernon Chinatown 3",
-        "thumbnail": "thumbnail3.jpg",
-        "summary": "Updated summary",
-        "published": true,
-        "cards": [
-          {
-            "media": "media.png",
-            "description": "an updated picture",
-          }
-        ]
-      })
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(sampleExhibitUpdated)
     },
     expectedReponseBodies: [PopulatedExhibitSchema]
-  },
+  }
+];
+
+const deleteTests: ApiTestProps[] = [
   {
     name: "Delete an Exhibit",
-    url: "/api/exhibit?",
+    url: `/api/exhibit?title=${encodeURIComponent(sampleExhibitUpdated.title)}`,
     request: {
       method: "DELETE",
       credentials: "same-origin"
     },
     expectedReponseBodies: [PopulatedExhibitSchema]
   },
+  {
+    name: "Delete a List of Exhibits",
+    url: `/api/exhibit?title=${encodeURIComponent(sampleExhibitUpdated.title)}&title=${encodeURIComponent(otherSampleExhibit.title)}`,
+    request: {
+      method: "DELETE",
+      credentials: "same-origin"
+    },
+    expectedReponseBodies: [z.array(PopulatedExhibitSchema)]
+  }
+]
+
+const tests: ApiTestProps[] = [
+  ...createTests,
+  ...readTests,
+  ...updateTests,
+  ...deleteTests
 ];
 
 export default tests;
